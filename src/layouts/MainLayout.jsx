0ed64c32863737a446bf5f7b87acc7a1cdaca184
kishormote <148129053+kishormote@users.noreply.github.com>
2025-01-09 19:40:15 +0530

    updates main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Component/Navbar";
-import Footer from "../Component/Footer";
+import Navbar from "../Component/navbar";
+import Footer from "../Component/footer";
 import { Box, Container } from "@mui/material";
 
 function MainLayout() {
